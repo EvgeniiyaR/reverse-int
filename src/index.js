88e987cb49067c05b15c7b1bba072112eb964685
EvgeniiyaR <tulpansik@mail.ru>
2023-01-25 00:27:28 +0300

    init: start task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
+  let reverse = '';
+  n = Math.abs(n);
+  for (let i = (String(n)).length - 1; i >= 0; i--) {
+	reverse += (String(n))[i];
+  }
+  return reverse;
 }
